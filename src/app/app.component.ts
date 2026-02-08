e38695c4e702d1ae848eb12050420464f1ffab46
27SHIVAMSUYAL <27shivam.1o1@gmail.com>
2026-02-09 01:41:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginCardComponent } from "./login-card/login-card.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginCardComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
